ee92635993e43a368d8608249c0655a50c0cc508
Tanya Staneva <33036767+tany1610@users.noreply.github.com>
2024-10-31 07:37:37 +0200

    add static files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+
+export const routes: Routes = [{
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+}];
